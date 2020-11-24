ef160c694fe7c089493a444fd6a9991c3a5dc962
Kainoa Keanaaina <kaikeanaaina@gmail.com>
2020-11-23 19:03:41 -1000

    setting up modeldetails page

---
+++ client/src/components/ModelDetails.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+export class ModelDetails extends Component {
+    render() {
+        return (
+            <div>
+                <div style={{textAlign:'center'}}>
+                    <h3>Model Details</h3>
+                </div>
+
+                <div>
+                    <p>content</p>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default ModelDetails
