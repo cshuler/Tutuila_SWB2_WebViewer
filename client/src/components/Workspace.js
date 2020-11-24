a6b74fdb5af05448f11e55eed1dcce455ee86ecc
Kainoa Keanaaina <kaikeanaaina@gmail.com>
2020-11-23 19:05:14 -1000

    initial map rendering

--- client/src/components/Workspace.js
+++ client/src/components/Workspace.js
@@ -1,15 +1,29 @@
 import React, { Component } from 'react'
 import { PieChart } from './PieChart'
+import {MapContainer, TileLayer, Popup, Marker} from 'react-leaflet'
 
 export class Workspace extends Component {
     render() {
         return (
             <div>
-                <div style={{textAlign:'center'}}>
+                <div style={{ textAlign: 'center' }}>
                     <h3>Workspace</h3>
                 </div>
                 <p>Modal agreement</p>
-                <p>Map Div</p>
+                <div>
+                    <p>Map Div</p>
+                    <MapContainer style={{ height: "360px" }} center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+                        <TileLayer
+                            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        />
+                        <Marker position={[51.505, -0.09]}>
+                            <Popup>
+                                A pretty CSS3 popup. <br /> Easily customizable.
+                            </Popup>
+                        </Marker>
+                    </MapContainer>
+                </div>
                 <PieChart />
                 <p>Layer Div</p>
                 <p>Base Case Scenario Div</p>
