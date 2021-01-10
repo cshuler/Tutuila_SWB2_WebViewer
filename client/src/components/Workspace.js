0659631ed830558e9bc0cc92673c2d723365953f
Kainoa Keanaaina <kaikeanaaina@gmail.com>
2021-01-09 18:39:25 -1000

    renders mapbox-gl map

--- client/src/components/Workspace.js
+++ client/src/components/Workspace.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import { PieChart } from './PieChart' 
 import Map from './Map'
+import MapBoxMap from './MapBoxMap'
 export class Workspace extends Component {
     
     render() {
         return (
             <div>
                 <Map />
+                <MapBoxMap />
                 <PieChart />
                 <p>Layer Div</p>
                 <p>Base Case Scenario Div</p>
