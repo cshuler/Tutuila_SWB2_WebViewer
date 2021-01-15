eb7861b4ccc7f93155860f88f9d89c026d5e18d3
Kainoa Keanaaina <kanakahacks@Kainoas-MacBook-Pro.local>
2021-01-15 12:20:32 -1000

    barebone temp

--- client/src/components/TempWS.js
+++ client/src/components/TempWS.js
@@ -3,7 +3,6 @@ import { MapContainer, TileLayer, Popup, Marker, LayersControl, Circle, LayerGro
 import "./MyMap.css"
 import chroma from 'chroma-js'
 // import geojsonvt from 'geojson-vt'
-import MapBoxMap from './MapBoxMap'
 
 
 const ETData = require('../data/geoJson_files/ET_prj_cleaned.json')
@@ -12,8 +11,6 @@ const rainFallData = require('../data/geoJson_files/rainfall_prj_cleaned.json')
 const rechargeData = require('../data/geoJson_files/recharge_prj_cleaned.json')
 const runOffData = require('../data/geoJson_files/runoff_prj_cleaned.json')
 
-
-// const gp = require("geojson-precision")
 
 const mapCenter = [-14.30, -170.70]
 const zoomLevel = 11;
@@ -71,80 +68,7 @@ export class TempWS extends Component {
                 <p>Modal agreement</p>
                 <div id="mapdiv">
                     <p>Map Div</p>
-                    <MapContainer style={{ height: "480px" }} center={mapCenter} zoom={zoomLevel} scrollWheelZoom={false}>
-
-                        <LayersControl position="topright">
-                            <LayersControl.BaseLayer checked style={this.cellStyle} name="RunOFF">
-
-                                <MapBoxMap />
-
-                            </LayersControl.BaseLayer>
-
-                            
-                            <LayersControl.BaseLayer name="OpenStreetMap.Mapnik">
-                                <TileLayer
-                                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                                />
-                            </LayersControl.BaseLayer>
-                            <LayersControl.BaseLayer name="OpenStreetMap.BlackAndWhite">
-                                <TileLayer
-                                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                                    url="https://tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png"
-                                />
-                            </LayersControl.BaseLayer>
-                            <LayersControl.BaseLayer name="OpenTopoMap">
-                                <TileLayer
-                                    attribution='Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
-                                    url="https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png"
-                                />
-                            </LayersControl.BaseLayer>
-                            <LayersControl.BaseLayer name="Esri.WorldImagery">
-                                <TileLayer
-                                    attribution='Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
-                                    url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
-                                />
-                            </LayersControl.BaseLayer>
-
-                            <LayersControl.Overlay name="Marker with popup">
-                                <Marker position={mapCenter}>
-                                    <Popup>
-                                        A pretty CSS3 popup. <br /> Easily customizable.
-                                    </Popup>
-                                </Marker>
-                            </LayersControl.Overlay>
-                            <LayersControl.Overlay name="Layer group with circles">
-                                <LayerGroup>
-                                    <Circle
-                                        center={mapCenter}
-                                        pathOptions={{ fillColor: 'blue' }}
-                                        radius={200}
-                                    />
-                                    <Circle
-                                        center={mapCenter}
-                                        pathOptions={{ fillColor: 'red' }}
-                                        radius={100}
-                                        stroke={false}
-                                    />
-                                    <LayerGroup>
-                                        <Circle
-                                            center={[51.51, -0.08]}
-                                            pathOptions={{ color: 'green', fillColor: 'green' }}
-                                            radius={100}
-                                        />
-                                    </LayerGroup>
-                                </LayerGroup>
-                            </LayersControl.Overlay>
-                            <LayersControl.Overlay name="Feature group">
-                                <FeatureGroup pathOptions={{ color: 'purple' }}>
-                                    <Popup>Popup in FeatureGroup</Popup>
-                                    <Circle center={[51.51, -0.06]} radius={200} />
-                                    <Rectangle bounds={rectangle} />
-                                </FeatureGroup>
-                            </LayersControl.Overlay>
-
-                        </LayersControl>
-                    </MapContainer>
+                    <p>temp</p>
                 </div>
             </div>
         )
