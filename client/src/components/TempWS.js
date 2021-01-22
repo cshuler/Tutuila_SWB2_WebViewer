9fa70b747a07d7a2474bc66038ff412cdd050a79
Kainoa Keanaaina <kaikeanaaina@gmail.com>
2021-01-21 21:10:38 -1000

    minZoom and removed dependencies

--- client/src/components/TempWS.js
+++ client/src/components/TempWS.js
@@ -1,8 +1,7 @@
-import React, { useState, useRef } from 'react'
+import React, { useState } from 'react'
 import ReactMapGL, { Source, Layer } from 'react-map-gl'
-import { MapContainer, TileLayer, LayersControl, Circle, LayerGroup, useMapEvents, GeoJSON, Marker } from 'react-leaflet'
 import "./MyMap.css"
-import chroma from 'chroma-js'
+//import chroma from 'chroma-js'
 import {dataLayer} from './map-style'
 const {REACT_APP_MAPBOX_ACESS_TOKEN} = require('../config/keys')
 
@@ -16,34 +15,9 @@ const {REACT_APP_MAPBOX_ACESS_TOKEN} = require('../config/keys')
 const runOffData = require('../data/geoJson_files/runoff_prj_cleaned.json')
 
 
-const mapCenter = [-14.30, -170.70]
-const zoomLevel = 11;
-var highestGridcode = 0
+//var highestGridcode = 0
 
 export default function TempWS() {
-    // cellStyle = (cell) => {
-    //     var gridcode = cell.properties.gridCode
-    //     // console.log(gridcode.toFixed(2))
-    //     if (gridcode > highestGridcode) {
-    //         highestGridcode = gridcode
-    //     }
-    //     if (gridcode === 0) {
-    //         return {
-    //             fillColor: 'black', color: 'black', fillOpacity: 1
-    //         }
-    //     }
-
-    //     var mapScale = chroma.scale(['blue', 'yellow', 'red'])
-    //         .domain([0, 180])
-
-    //     var fillColor = mapScale(gridcode)
-
-    //     return {
-    //         fillColor: fillColor,
-    //         color: fillColor,
-    //         fillOpacity: 1
-    //     }
-    // }
     const [viewport, setViewport] = useState({
         latitude: -14.30,
         longitude: -170.70,
@@ -51,12 +25,12 @@ export default function TempWS() {
         height: "500px",
         zoom: 10.5,
       });
-    const mapRef = useRef();
 
     return (
         <ReactMapGL
           {...viewport}
           maxZoom={20}
+          minZoom={5}
           mapboxApiAccessToken={REACT_APP_MAPBOX_ACESS_TOKEN}
           onViewportChange={(newViewport) => {
             setViewport({ ...newViewport} )
