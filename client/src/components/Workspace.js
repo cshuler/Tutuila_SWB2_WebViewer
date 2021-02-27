e0c017b523ef605113254c18447ebdc19d326622
Kainoa Keanaaina <kanakahacks@Kainoas-MacBook-Pro.local>
2021-02-26 20:20:05 -1000

    styling migration

--- client/src/components/Workspace.js
+++ client/src/components/Workspace.js
@@ -12,10 +12,38 @@ const ETData = require("../data/geoJson_files/ET_prj_cleaned.json");
 const interceptionData = require("../data/geoJson_files/interception_prj_cleaned.json");
 const rechargeData = require("../data/geoJson_files/recharge_prj_cleaned.json");
 
-const defaultLabelStyle = {
-  fontSize: "10px",
-  fontFamily: "sans-serif"
-};
+const styles = {
+  pieChartFont: {
+    fontSize: "10px",
+    fontFamily: "sans-serif"
+  },
+  mapCpontainer: {
+    marginTop: "10px"
+  },
+  pieChartContainer: {
+    width: 200,
+    height: 200,
+    display: "flext",
+    justifyContent: "center",
+    alignItems: "center",
+    marginLeft: 100,
+  },
+  sliderContainer: {
+    width: 100
+  },
+  selectedMapHeader: {
+    textAlign: "center"
+  },
+  buttonContainer: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  changeMapButton: {
+    margin: "5px",
+  }
+}
+
 export default class Workspace extends Component {
   constructor(props) {
     super(props);
@@ -23,8 +51,8 @@ export default class Workspace extends Component {
       viewport: {
         latitude: -14.3,
         longitude: -170.7,
-        width: "100%",
-        height: "500px",
+        width: "745px",
+        height: "490px",
         zoom: 10.5,
       },
       viewport2: {
@@ -165,7 +193,7 @@ export default class Workspace extends Component {
 
     this.setState({
       pieDataArray: [
-        { title: `R.O.`, value: runOffGridCodeTotal, percentage:`${runOffPercentage}%`,  color: "#E38627" },
+        { title: `R.O.`, value: runOffGridCodeTotal, percentage: `${runOffPercentage}%`, color: "#E38627" },
         { title: `ET`, value: ETGridCodeTotal, percentage: `${ETPercentage}%`, color: "#01BDD3" },
         {
           title: `Int.`,
@@ -175,45 +203,39 @@ export default class Workspace extends Component {
         },
         { title: `Recharge`, value: rechargeGridCodeTotal, percentage: `${rechargePercentage}%`, color: "#78BCED" },
       ],
-      viewport: newViewport 
+      viewport: newViewport
     });
     // return console.log(this.state.pieDataArray)
   }
   render() {
     return (
       <div>
-        <ReactMapGL
-          {...this.state.viewport}
-          maxZoom={18}
-          minZoom={10.5}
-          mapboxApiAccessToken={REACT_APP_MAPBOX_ACCESS_TOKEN}
-          onViewportChange={(newViewport) => {
-            this.check(newViewport)
-          }}
-          mapStyle={"mapbox://styles/kanakahacks/ckkkwbaag37w017p665v22142"}
-          attributionControl={false}
-          style={{ marginTop: "10px" }}
-        >
+        <div style={styles.mapCpontainer}>
           <ReactMapGL
             {...this.state.viewport}
             maxZoom={18}
             minZoom={10.5}
             mapboxApiAccessToken={REACT_APP_MAPBOX_ACCESS_TOKEN}
-            // onMouseUp={this.check}
-            mapStyle={this.state.mapStylingStyle}
-            style={{ opacity: this.state.opacityValuePercentage }}
-          ></ReactMapGL>
-        </ReactMapGL>
+            onViewportChange={(newViewport) => {
+              this.check(newViewport)
+            }}
+            mapStyle={"mapbox://styles/kanakahacks/ckkkwbaag37w017p665v22142"}
+            attributionControl={false}
+          >
+            <ReactMapGL
+              {...this.state.viewport}
+              maxZoom={18}
+              minZoom={10.5}
+              mapboxApiAccessToken={REACT_APP_MAPBOX_ACCESS_TOKEN}
+              // onMouseUp={this.check}
+              mapStyle={this.state.mapStylingStyle}
+              style={{ opacity: this.state.opacityValuePercentage }}
+            ></ReactMapGL>
+          </ReactMapGL>
+        </div>
 
         <div
-          style={{
-            width: 200,
-            height: 200,
-            display: "flext",
-            justifyContent: "center",
-            alignItems: "center",
-            marginLeft: 100,
-          }}
+          style={styles.pieChartContainer}
         >
           <PieChart
             data={this.state.pieDataArray}
@@ -221,7 +243,7 @@ export default class Workspace extends Component {
             lineWidth={40}
             label={({ dataEntry }) => `${dataEntry.title}: ${dataEntry.percentage}`}
             labelStyle={{
-              ...defaultLabelStyle,
+              ...styles.pieChartFont,
             }}
             animate={true}
             viewBoxSize={[125, 125]}
@@ -230,107 +252,99 @@ export default class Workspace extends Component {
           />
         </div>
 
-        <Slider
-          value={this.state.opacityValueLevel}
-          onChange={(event, newValue) => {
-            this.setState({
-              opacityValueLevel: newValue,
-              opacityValuePercentage: `${newValue}%`,
-            });
-          }}
-          aria-labelledby="continuous-slider"
-        />
-        <p>{this.state.opacityValueLevel}</p>
-        <h1 style={{ textAlign: "center" }}>{this.state.selectedMap}</h1>
+        <div style={styles.sliderContainer}>
 
-        <div
-          id="buttonsDiv"
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <Button
-            variant="outlined"
-            color="primary"
-            style={{
-              margin: "5px",
-            }}
-            onClick={() => {
+          <Slider
+            value={this.state.opacityValueLevel}
+            onChange={(event, newValue) => {
               this.setState({
-                selectedMap: "Run Off",
-                mapStylingStyle:
-                  "mapbox://styles/kanakahacks/ckkex8tti0fni17qpb5vhmjd2",
+                opacityValueLevel: newValue,
+                opacityValuePercentage: `${newValue}%`,
               });
             }}
+            aria-labelledby="continuous-slider"
+          />
+          <p>{this.state.opacityValueLevel}</p>
+        </div>
+        <div>
+
+
+          <h1 style={styles.selectedMapHeader}>{this.state.selectedMap}</h1>
+          <div
+            id="buttonsDiv"
+            style={styles.buttonContainer}
           >
-            Run Off
+            <Button
+              variant="outlined"
+              color="primary"
+              style={styles.changeMapButton}
+              onClick={() => {
+                this.setState({
+                  selectedMap: "Run Off",
+                  mapStylingStyle:
+                    "mapbox://styles/kanakahacks/ckkex8tti0fni17qpb5vhmjd2",
+                });
+              }}
+            >
+              Run Off
           </Button>
-          <Button
-            variant="outlined"
-            color="primary"
-            style={{
-              margin: "5px",
-            }}
-            onClick={() => {
-              this.setState({
-                selectedMap: "Recharge",
-                mapStylingStyle:
-                  "mapbox://styles/kanakahacks/ckkj3885701qs18lcfdyms3k0",
-              });
-            }}
-          >
-            Recharge
+            <Button
+              variant="outlined"
+              color="primary"
+              style={styles.changeMapButton}
+              onClick={() => {
+                this.setState({
+                  selectedMap: "Recharge",
+                  mapStylingStyle:
+                    "mapbox://styles/kanakahacks/ckkj3885701qs18lcfdyms3k0",
+                });
+              }}
+            >
+              Recharge
           </Button>
-          <Button
-            variant="outlined"
-            color="primary"
-            style={{
-              margin: "5px",
-            }}
-            onClick={() => {
-              this.setState({
-                selectedMap: "Interception",
-                mapStylingStyle:
-                  "mapbox://styles/kanakahacks/ckkj4fpf902h617o0nbcyy6yi",
-              });
-            }}
-          >
-            Interception
+            <Button
+              variant="outlined"
+              color="primary"
+              style={styles.changeMapButton}
+              onClick={() => {
+                this.setState({
+                  selectedMap: "Interception",
+                  mapStylingStyle:
+                    "mapbox://styles/kanakahacks/ckkj4fpf902h617o0nbcyy6yi",
+                });
+              }}
+            >
+              Interception
           </Button>
-          <Button
-            variant="outlined"
-            color="primary"
-            style={{
-              margin: "5px",
-            }}
-            onClick={() => {
-              this.setState({
-                selectedMap: "ET",
-                mapStylingStyle:
-                  "mapbox://styles/kanakahacks/ckkj59vqf1if617lnmh73qaj1",
-              });
-            }}
-          >
-            ET
+            <Button
+              variant="outlined"
+              color="primary"
+              style={styles.changeMapButton}
+              onClick={() => {
+                this.setState({
+                  selectedMap: "ET",
+                  mapStylingStyle:
+                    "mapbox://styles/kanakahacks/ckkj59vqf1if617lnmh73qaj1",
+                });
+              }}
+            >
+              ET
           </Button>
-          <Button
-            variant="outlined"
-            color="primary"
-            style={{
-              margin: "5px",
-            }}
-            onClick={() => {
-              this.setState({
-                selectedMap: "Rainfall",
-                mapStylingStyle:
-                  "mapbox://styles/kanakahacks/ckkj69k4b111k17mn3wz0071p",
-              });
-            }}
-          >
-            Rainfall
+            <Button
+              variant="outlined"
+              color="primary"
+              style={styles.changeMapButton}
+              onClick={() => {
+                this.setState({
+                  selectedMap: "Rainfall",
+                  mapStylingStyle:
+                    "mapbox://styles/kanakahacks/ckkj69k4b111k17mn3wz0071p",
+                });
+              }}
+            >
+              Rainfall
           </Button>
+          </div>
         </div>
       </div>
     );
