6f571f607eb66b91337511d324bb0d0a6eaf5100
Kainoa Keanaaina <kaikeanaaina@gmail.com>
2021-03-03 18:17:17 -1000

    refactoring code. pieChartMap does not update when map moves

--- client/src/components/PieChartMap.js
+++ client/src/components/PieChartMap.js
@@ -1,22 +1,171 @@
 import React, { Component } from "react";
 import { PieChart } from "react-minimal-pie-chart";
-import ReactMapGL, { WebMercatorViewport } from "react-map-gl";
+import { WebMercatorViewport } from "react-map-gl";
+
+const runOffData = require("../data/geoJson_files/runoff_prj_cleaned.json");
+const ETData = require("../data/geoJson_files/ET_prj_cleaned.json");
+const interceptionData = require("../data/geoJson_files/interception_prj_cleaned.json");
+const rechargeData = require("../data/geoJson_files/recharge_prj_cleaned.json");
 
 const styles = {
   pieChartFont: {
     fontSize: "10px",
-    fontFamily: "sans-serif"
-  }
-}
+    fontFamily: "sans-serif",
+  },
+  pieContainer: {
+    width: 300,
+    height: 200,
+  },
+};
 
 export default class PieChartMap extends Component {
-  construtor(props) {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pieDataArray: [],
+    };
+  }
+  componentDidMount() {
+    //this gets the bounds of the viewer
+    const vp = new WebMercatorViewport(this.props.viewport);
+    const theBounds = vp.getBounds();
+    const westBound = Number(theBounds[0][0]);
+    const southBound = Number(theBounds[0][1]);
+    const eastBound = Number(theBounds[1][0]);
+    const northBound = Number(theBounds[1][1]);
+
+    ///////////////////////////////////////////////
+    var runOffGridCodeTotal = 0;
+
+    const runOffFilteredData = runOffData.features
+      .filter(
+        (feature) => Number(feature.geometry.coordinates[0][1][0]) <= eastBound
+      )
+      .filter(
+        (feature) => Number(feature.geometry.coordinates[0][1][1]) <= northBound
+      )
+      .filter(
+        (feature) => Number(feature.geometry.coordinates[0][3][0]) >= westBound
+      )
+      .filter((feature) =>
+        Number(feature.geometry.coordinates[0][3][1] >= southBound)
+      );
+
+    runOffFilteredData.forEach((feature) => {
+      runOffGridCodeTotal += Number(feature.properties.gridCode);
+    });
+
+    //////////////////////////////////////////////////////
+
+    var ETGridCodeTotal = 0;
+
+    const ETFilteredData = ETData.features
+      .filter(
+        (feature) => Number(feature.geometry.coordinates[0][1][0]) <= eastBound
+      )
+      .filter(
+        (feature) => Number(feature.geometry.coordinates[0][1][1]) <= northBound
+      )
+      .filter(
+        (feature) => Number(feature.geometry.coordinates[0][3][0]) >= westBound
+      )
+      .filter((feature) =>
+        Number(feature.geometry.coordinates[0][3][1] >= southBound)
+      );
+
+    ETFilteredData.forEach((feature) => {
+      ETGridCodeTotal += Number(feature.properties.gridCode);
+    });
+
+    ///////////////////////////////////////////////
+    var interceptionGridCodeTotal = 0;
+
+    const interceptionFilteredData = interceptionData.features
+      .filter(
+        (feature) => Number(feature.geometry.coordinates[0][1][0]) <= eastBound
+      )
+      .filter(
+        (feature) => Number(feature.geometry.coordinates[0][1][1]) <= northBound
+      )
+      .filter(
+        (feature) => Number(feature.geometry.coordinates[0][3][0]) >= westBound
+      )
+      .filter((feature) =>
+        Number(feature.geometry.coordinates[0][3][1] >= southBound)
+      );
+
+    interceptionFilteredData.forEach((feature) => {
+      interceptionGridCodeTotal += Number(feature.properties.gridCode);
+    });
+    ////////////////////////////////////////////////////////////////////
+    var rechargeGridCodeTotal = 0;
+
+    const rechargeFilteredData = rechargeData.features
+      .filter(
+        (feature) => Number(feature.geometry.coordinates[0][1][0]) <= eastBound
+      )
+      .filter(
+        (feature) => Number(feature.geometry.coordinates[0][1][1]) <= northBound
+      )
+      .filter(
+        (feature) => Number(feature.geometry.coordinates[0][3][0]) >= westBound
+      )
+      .filter((feature) =>
+        Number(feature.geometry.coordinates[0][3][1] >= southBound)
+      );
+
+    rechargeFilteredData.forEach((feature) => {
+      rechargeGridCodeTotal += Number(feature.properties.gridCode);
+    });
+
+    var sumOfData =
+      runOffGridCodeTotal +
+      ETGridCodeTotal +
+      interceptionGridCodeTotal +
+      rechargeGridCodeTotal;
+    var runOffPercentage = Math.floor((runOffGridCodeTotal / sumOfData) * 100);
+    var ETPercentage = Math.floor((ETGridCodeTotal / sumOfData) * 100);
+    var InterceptionPercentage = Math.floor(
+      (interceptionGridCodeTotal / sumOfData) * 100
+    );
+    var rechargePercentage = Math.floor(
+      (rechargeGridCodeTotal / sumOfData) * 100
+    );
+
+    this.setState({
+      pieDataArray: [
+        {
+          title: `R.O.`,
+          value: runOffGridCodeTotal,
+          percentage: `${runOffPercentage}%`,
+          color: "#E38627",
+        },
+        {
+          title: `ET`,
+          value: ETGridCodeTotal,
+          percentage: `${ETPercentage}%`,
+          color: "#01BDD3",
+        },
+        {
+          title: `Int.`,
+          value: interceptionGridCodeTotal,
+          percentage: `${InterceptionPercentage}%`,
+          color: "#71F523",
+        },
+        {
+          title: `Recharge`,
+          value: rechargeGridCodeTotal,
+          percentage: `${rechargePercentage}%`,
+          color: "#78BCED",
+        },
+      ],
+    });
   }
   render() {
     return (
-      <div>
+      <div style={styles.pieContainer}>
         <PieChart
-          data={this.props.pieDataArray}
+          data={this.state.pieDataArray}
           paddingAngle={5}
           lineWidth={40}
           label={({ dataEntry }) =>
--- client/src/components/Workspace.js
+++ client/src/components/Workspace.js
@@ -88,147 +88,9 @@ export default class Workspace extends Component {
         ],
       },
     };
-    this.check = this.check.bind(this);
     this.changeMap = this.changeMap.bind(this);
   }
-  check(newViewport) {
-    //this gets the bounds of the viewer
-    const vp = new WebMercatorViewport(newViewport);
-    const theBounds = vp.getBounds();
-    const westBound = Number(theBounds[0][0]);
-    const southBound = Number(theBounds[0][1]);
-    const eastBound = Number(theBounds[1][0]);
-    const northBound = Number(theBounds[1][1]);
-
-    ///////////////////////////////////////////////
-    var runOffGridCodeTotal = 0;
-
-    const runOffFilteredData = runOffData.features
-      .filter(
-        (feature) => Number(feature.geometry.coordinates[0][1][0]) <= eastBound
-      )
-      .filter(
-        (feature) => Number(feature.geometry.coordinates[0][1][1]) <= northBound
-      )
-      .filter(
-        (feature) => Number(feature.geometry.coordinates[0][3][0]) >= westBound
-      )
-      .filter((feature) =>
-        Number(feature.geometry.coordinates[0][3][1] >= southBound)
-      );
-
-    runOffFilteredData.forEach((feature) => {
-      runOffGridCodeTotal += Number(feature.properties.gridCode);
-    });
-
-    //////////////////////////////////////////////////////
-
-    var ETGridCodeTotal = 0;
-
-    const ETFilteredData = ETData.features
-      .filter(
-        (feature) => Number(feature.geometry.coordinates[0][1][0]) <= eastBound
-      )
-      .filter(
-        (feature) => Number(feature.geometry.coordinates[0][1][1]) <= northBound
-      )
-      .filter(
-        (feature) => Number(feature.geometry.coordinates[0][3][0]) >= westBound
-      )
-      .filter((feature) =>
-        Number(feature.geometry.coordinates[0][3][1] >= southBound)
-      );
-
-    ETFilteredData.forEach((feature) => {
-      ETGridCodeTotal += Number(feature.properties.gridCode);
-    });
-
-    ///////////////////////////////////////////////
-    var interceptionGridCodeTotal = 0;
-
-    const interceptionFilteredData = interceptionData.features
-      .filter(
-        (feature) => Number(feature.geometry.coordinates[0][1][0]) <= eastBound
-      )
-      .filter(
-        (feature) => Number(feature.geometry.coordinates[0][1][1]) <= northBound
-      )
-      .filter(
-        (feature) => Number(feature.geometry.coordinates[0][3][0]) >= westBound
-      )
-      .filter((feature) =>
-        Number(feature.geometry.coordinates[0][3][1] >= southBound)
-      );
-
-    interceptionFilteredData.forEach((feature) => {
-      interceptionGridCodeTotal += Number(feature.properties.gridCode);
-    });
-    ////////////////////////////////////////////////////////////////////
-    var rechargeGridCodeTotal = 0;
-
-    const rechargeFilteredData = rechargeData.features
-      .filter(
-        (feature) => Number(feature.geometry.coordinates[0][1][0]) <= eastBound
-      )
-      .filter(
-        (feature) => Number(feature.geometry.coordinates[0][1][1]) <= northBound
-      )
-      .filter(
-        (feature) => Number(feature.geometry.coordinates[0][3][0]) >= westBound
-      )
-      .filter((feature) =>
-        Number(feature.geometry.coordinates[0][3][1] >= southBound)
-      );
-
-    rechargeFilteredData.forEach((feature) => {
-      rechargeGridCodeTotal += Number(feature.properties.gridCode);
-    });
-
-    var sumOfData =
-      runOffGridCodeTotal +
-      ETGridCodeTotal +
-      interceptionGridCodeTotal +
-      rechargeGridCodeTotal;
-    var runOffPercentage = Math.floor((runOffGridCodeTotal / sumOfData) * 100);
-    var ETPercentage = Math.floor((ETGridCodeTotal / sumOfData) * 100);
-    var InterceptionPercentage = Math.floor(
-      (interceptionGridCodeTotal / sumOfData) * 100
-    );
-    var rechargePercentage = Math.floor(
-      (rechargeGridCodeTotal / sumOfData) * 100
-    );
-
-    this.setState({
-      pieDataArray: [
-        {
-          title: `R.O.`,
-          value: runOffGridCodeTotal,
-          percentage: `${runOffPercentage}%`,
-          color: "#E38627",
-        },
-        {
-          title: `ET`,
-          value: ETGridCodeTotal,
-          percentage: `${ETPercentage}%`,
-          color: "#01BDD3",
-        },
-        {
-          title: `Int.`,
-          value: interceptionGridCodeTotal,
-          percentage: `${InterceptionPercentage}%`,
-          color: "#71F523",
-        },
-        {
-          title: `Recharge`,
-          value: rechargeGridCodeTotal,
-          percentage: `${rechargePercentage}%`,
-          color: "#78BCED",
-        },
-      ],
-      viewport: newViewport,
-    });
-    // return console.log(this.state.pieDataArray)
-  }
+  
   changeMap(selectedMap, mapStylingStyle) {
     this.setState({ selectedMap, mapStylingStyle });
   }
@@ -242,7 +104,7 @@ export default class Workspace extends Component {
             minZoom={10.5}
             mapboxApiAccessToken={REACT_APP_MAPBOX_ACCESS_TOKEN}
             onViewportChange={(newViewport) => {
-              this.check(newViewport);
+              this.setState({viewport: newViewport})
             }}
             mapStyle={"mapbox://styles/kanakahacks/ckkkwbaag37w017p665v22142"}
             attributionControl={false}
@@ -260,7 +122,7 @@ export default class Workspace extends Component {
         </div>
 
         <div>
-          <PieChartMap pieDataArray={this.state.pieDataArray} />
+          <PieChartMap viewport={this.state.viewport} />
         </div>
 
         <div style={styles.sliderContainer}>
