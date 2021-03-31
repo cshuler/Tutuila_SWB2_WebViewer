86cee769b539226186dc577746b7bb0be0e9dbd3
Kainoa Keanaaina <kaikeanaaina@gmail.com>
2021-03-30 22:37:57 -1000

    legend added to the bottom of the map as a image

--- client/src/components/Workspace.js
+++ client/src/components/Workspace.js
@@ -1,17 +1,23 @@
 import React, { Component } from "react";
 import ReactMapGL from "react-map-gl";
-import { Slider } from '@material-ui/core';
-import Modal from './WorkspaceModal'
+import { Slider } from "@material-ui/core";
+import Modal from "./WorkspaceModal";
 
 import PieChartMap from "./PieChartMap";
 import MapRadio from "./MapRadio";
+
+import runOffLegend from "../assets/runOffLegend.png";
+import rechargeLegend from "../assets/rechargeLegend.png";
+import interceptionLegend from "../assets/interceptionLegend.png";
+import ETLegend from "../assets/ETLegend.png";
+import rainfallLegend from "../assets/rainfallLegend.png";
 
 const { REACT_APP_MAPBOX_ACCESS_TOKEN } = require("../config/keys");
 
 const styles = {
   workSpaceContainer: {
     display: "flex",
-    justifyContent: "center"
+    justifyContent: "center",
   },
   mapCpontainer: {
     marginTop: "10px",
@@ -25,7 +31,7 @@ const styles = {
     display: "flext",
     justifyContent: "center",
     alignItems: "center",
-    marginLeft:10,
+    marginLeft: 10,
   },
   sliderContainer: {
     width: 100,
@@ -45,11 +51,15 @@ const styles = {
     display: "flex",
     flexDirection: "column",
     paddingLeft: 10,
-    width: 200
+    width: 200,
   },
   opacityLevel: {
-    width: 150
-  }
+    width: 150,
+  },
+  legendImg: {
+    display: "flex",
+    justifyContent: "center",
+  },
 };
 
 export default class Workspace extends Component {
@@ -70,20 +80,46 @@ export default class Workspace extends Component {
       opacityValuePercentage: "100%",
       pieDataArray: [],
       gifts: [],
-      modalIsOpen: true
+      modalIsOpen: true,
     };
     this.changeMap = this.changeMap.bind(this);
-    this.handleOpenModal = this.handleOpenModal.bind(this)
-    this.handleCloseModal = this.handleCloseModal.bind(this)
+    this.handleOpenModal = this.handleOpenModal.bind(this);
+    this.handleCloseModal = this.handleCloseModal.bind(this);
+    this.renderLegend = this.renderLegend.bind(this)
   }
   changeMap(selectedMap, mapStylingStyle) {
     this.setState({ selectedMap, mapStylingStyle });
   }
-  handleOpenModal () {
-    this.setState({modalIsOpen: true})
+  handleOpenModal() {
+    this.setState({ modalIsOpen: true });
   }
-  handleCloseModal () {
-    this.setState({modalIsOpen: false})
+  handleCloseModal() {
+    this.setState({ modalIsOpen: false });
+  }
+  renderLegend() {
+
+    let theLegend = null 
+
+    if (this.state.selectedMap === "Runoff") {
+      theLegend = runOffLegend;
+    } else if (this.state.selectedMap === "Recharge") {
+      theLegend = rechargeLegend;
+    } else if (this.state.selectedMap === "ET") {
+      theLegend = ETLegend;
+    } else if (this.state.selectedMap === "Rainfall") {
+      theLegend = rainfallLegend;
+    } else if (this.state.selectedMap === "Interception") {
+      theLegend = interceptionLegend;
+    } else {
+      theLegend = null;
+    }
+
+    return <img
+      src={theLegend}
+      alt="Girl in a jacket"
+      width="500"
+      height="100"
+    />;
   }
   render() {
     return (
@@ -92,30 +128,38 @@ export default class Workspace extends Component {
           <PieChartMap viewport={this.state.viewport} />
         </div>
 
-        <Modal modalIsOpen={this.state.modalIsOpen} handleOpen={this.handleOpenModal} handleClose={this.handleCloseModal} />
+        <Modal
+          modalIsOpen={this.state.modalIsOpen}
+          handleOpen={this.handleOpenModal}
+          handleClose={this.handleCloseModal}
+        />
 
-        <div style={styles.mapCpontainer}>
-          <ReactMapGL
-            {...this.state.viewport}
-            maxZoom={18}
-            minZoom={10.5}
-            mapboxApiAccessToken={REACT_APP_MAPBOX_ACCESS_TOKEN}
-            onViewportChange={(newViewport) => {
-              this.setState({ viewport: newViewport })
-            }}
-            mapStyle={"mapbox://styles/kanakahacks/ckkkwbaag37w017p665v22142"}
-            attributionControl={false}
-          >
+        <div>
+          <div style={styles.mapCpontainer}>
             <ReactMapGL
               {...this.state.viewport}
               maxZoom={18}
               minZoom={10.5}
               mapboxApiAccessToken={REACT_APP_MAPBOX_ACCESS_TOKEN}
-              // onMouseUp={this.check}
-              mapStyle={this.state.mapStylingStyle}
-              style={{ opacity: this.state.opacityValuePercentage }}
-            ></ReactMapGL>
-          </ReactMapGL>
+              onViewportChange={(newViewport) => {
+                this.setState({ viewport: newViewport });
+              }}
+              mapStyle={"mapbox://styles/kanakahacks/ckkkwbaag37w017p665v22142"}
+              attributionControl={false}
+            >
+              <ReactMapGL
+                {...this.state.viewport}
+                maxZoom={18}
+                minZoom={10.5}
+                mapboxApiAccessToken={REACT_APP_MAPBOX_ACCESS_TOKEN}
+                // onMouseUp={this.check}
+                mapStyle={this.state.mapStylingStyle}
+                style={{ opacity: this.state.opacityValuePercentage }}
+              ></ReactMapGL>
+            </ReactMapGL>
+            <br></br>
+          </div>
+          <div style={styles.legendImg}>{this.renderLegend()}</div>
         </div>
 
         <div style={styles.rightColumn}>
@@ -124,7 +168,10 @@ export default class Workspace extends Component {
           </div>
 
           <div>
-            <MapRadio changeMap={this.changeMap} selectedMap={this.state.selectedMap} />
+            <MapRadio
+              changeMap={this.changeMap}
+              selectedMap={this.state.selectedMap}
+            />
           </div>
           <br></br>
 
@@ -139,9 +186,10 @@ export default class Workspace extends Component {
               }}
               aria-labelledby="continuous-slider"
             />
-            <p style={styles.opacityLevel}>Opacity Level: {this.state.opacityValueLevel}</p>
+            <p style={styles.opacityLevel}>
+              Opacity Level: {this.state.opacityValueLevel}
+            </p>
           </div>
-
         </div>
       </div>
     );
