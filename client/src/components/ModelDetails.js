f1c97d65e7f507843c715359baa0546f52279720
Kainoa Keanaaina <kanakahacks@Kainoas-MacBook-Pro.local>
2021-04-21 10:47:34 -1000

    updated first image to be able to pop to full screen

--- client/src/components/ModelDetails.js
+++ client/src/components/ModelDetails.js
@@ -2,24 +2,7 @@ import React, { Component } from 'react'
 import tut_map from '../assets/Fig1.jpg';
 import scematic from '../assets/scematic.JPG';
 import imps from '../assets/input_datas.png';
-import { makeStyles } from '@material-ui/core/styles';
-import Modal from '@material-ui/core/Modal';
-import Backdrop from '@material-ui/core/Backdrop';
-import Fade from '@material-ui/core/Fade';
-
-const useStyles = makeStyles((theme) => ({
-    modal: {
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    paper: {
-        backgroundColor: theme.palette.background.paper,
-        border: '2px solid #000',
-        boxShadow: theme.shadows[5],
-        padding: theme.spacing(2, 4, 3),
-    },
-}));
+import TutImageModal from './ImageModal/TutImageModal'
 
 const styles = {
     modelHeader: {
@@ -35,7 +18,6 @@ const styles = {
         width: "300"
     }
 }
-
 
 export class ModelDetails extends Component {
     constructor(props) {
@@ -66,30 +48,7 @@ export class ModelDetails extends Component {
 
                         <div className="row">
                             <div className="map" style={styles.mapImageContainer}>
-                                <button type="button" onClick={this.handleOpen}>
-                                    react-transition-group
-                                </button>
-                                <Modal
-                                    aria-labelledby="transition-modal-title"
-                                    aria-describedby="transition-modal-description"
-                                    className={useStyles.modal}
-                                    open={this.state.isOpen}
-                                    onClose={this.handleClose}
-                                    closeAfterTransition
-                                    BackdropComponent={Backdrop}
-                                    BackdropProps={{
-                                        timeout: 500,
-                                    }}
-                                >
-                                    <Fade in={this.state.isOpen}>
-                                        <div className={useStyles.paper}>
-                                            <h2 id="transition-modal-title">Transition modal</h2>
-                                            <p id="transition-modal-description">react-transition-group animates me.</p>
-                                        </div>
-                                    </Fade>
-                                </Modal>
-                                <img src={tut_map} width="500" height="250" alt='map' />
-                                <br></br>
+                                <TutImageModal src={tut_map}/>                             
                                 <i>Map of Tutuila Island</i>
                             </div>
                         </div>
