c394d824780f77172cb726c16f1592052f5ee03d
Kainoa Keanaaina <kanakahacks@Kainoas-MacBook-Pro.local>
2021-05-10 23:46:02 -1000

    initializing antd for different navbar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,33 +1,70 @@
-import React from 'react'
-import { Navbar, Nav } from 'react-bootstrap';
+// import React from 'react'
+// import { Navbar, Nav } from 'react-bootstrap';
 
-export default function NavigationBar (){
-        return (
-            <div>
+// export default function NavigationBar (){
+//         return (
+//             <div>
                
-                <Navbar bg='primary' variant="dark" >
+//                 <Navbar bg='primary' variant="dark" >
 
-                    <Navbar.Brand href="/" eventkey="home">Tutuila</Navbar.Brand>
-                    <Nav justify variant="pill">
-                        <Nav.Item>
-                            <Nav.Link href="/workspace" eventKey="workspace" >Workspace</Nav.Link>
-                        </Nav.Item>
-                        <Nav.Item>
-                            <Nav.Link href="/modeldetails" eventKey="modeldetails" >Details</Nav.Link>
-                        </Nav.Item>
-                        <Nav.Item>
-                            <Nav.Link href="/background" eventKey="background" >Background</Nav.Link>
-                        </Nav.Item>
-                        <Nav.Item>
-                            <Nav.Link href="/about" eventKey="about" >AboutApp</Nav.Link>
-                        </Nav.Item>
-                    </Nav>
-                </Navbar>
+//                     <Navbar.Brand href="/" eventkey="home">Tutuila</Navbar.Brand>
+//                     <Nav justify variant="pill">
+//                         <Nav.Item>
+//                             <Nav.Link href="/workspace" eventKey="workspace" >Workspace</Nav.Link>
+//                         </Nav.Item>
+//                         <Nav.Item>
+//                             <Nav.Link href="/modeldetails" eventKey="modeldetails" >Details</Nav.Link>
+//                         </Nav.Item>
+//                         <Nav.Item>
+//                             <Nav.Link href="/background" eventKey="background" >Background</Nav.Link>
+//                         </Nav.Item>
+//                         <Nav.Item>
+//                             <Nav.Link href="/about" eventKey="about" >AboutApp</Nav.Link>
+//                         </Nav.Item>
+//                     </Nav>
+//                 </Navbar>
    
                     
                 
 
-            </div>
-        )
+//             </div>
+//         )
     
+// }
+
+import React from 'react'
+import { Menu } from 'antd';
+import { MailOutlined } from '@ant-design/icons';
+import 'antd/dist/antd.css'
+
+const { SubMenu } = Menu;
+
+export default class NavigationBar extends React.Component {
+  state = {
+    current: 'mail',
+  };
+
+  handleClick = e => {
+    console.log('click ', e);
+    this.setState({ current: e.key });
+  };
+
+  render() {
+    const { current } = this.state;
+    return (
+      <Menu onClick={this.handleClick} selectedKeys={[current]} mode="horizontal">
+        <Menu.Item key="home" icon={<MailOutlined />}>
+            <a href="/" >
+          Home
+            </a>
+        </Menu.Item>
+        <Menu.Item key="workspace">
+          <a href="/workspace">
+            Workspace
+          </a>
+        </Menu.Item>
+
+      </Menu>
+    );
+  }
 }
