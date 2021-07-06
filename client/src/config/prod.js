4491e1b7d6cce2544ef3ae45eb1f4942f5761dbe
Kainoa Keanaaina <kaikeanaaina@gmail.com>
2021-07-05 22:56:37 -1000

    updated instruction"

--- client/src/config/prod.js
+++ client/src/config/prod.js
@@ -1,4 +1,6 @@
 // prod.js - production keys here!!!
 module.exports = {
     REACT_APP_MAPBOX_ACCESS_TOKEN: process.env.REACT_APP_MAPBOX_ACCESS_TOKEN,
+    mongoURI:'mongodb+srv://USER:<password>@<sandboxID>.mongodb.net/test?retryWrites=true&w=majority',
+    atlasSecret: 'your atlas Secret Code Here',
   }
